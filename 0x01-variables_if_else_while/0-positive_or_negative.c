ac9ea53731d761e5425b8446cc136c717a795913
Incredibletochi <victor.oraekwuotu@gmail.com>
2022-07-02 03:17:34 -0700

    0. Positive anything is better

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,8 @@
 
 /**
  * main -> assign a random number to the variable n each time it is executed and prints out
- * based a condition
- * Return: always 0
+ *based on a condition
+ *Return: always o
  */
 int main(void)
 {
@@ -17,8 +17,6 @@ int main(void)
 	if (n == 0)
 		printf("%d is zero\n", n);
 	if (n < 0)
-	{
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
