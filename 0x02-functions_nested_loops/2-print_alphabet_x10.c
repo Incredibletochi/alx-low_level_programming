b36d42047c669b874f765064aca4d9854e87d917
Incredibletochi <victor.oraekwuotu@gmail.com>
2022-07-05 12:15:46 -0700

    I don tire for this task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * Prints_alphabet_x10 -> Prints the lowercase alphabets
+ * main - Entry point
+ * Description: Prints_alphabet_x10 -> prints the lowercase alphabets
  */
+
 void print_alphabet_x10(void);
 {
-	int i;
+	int j;
 	int x;
 
 	for (x = 0; x < 10; x++)
 	{
-		for  (i = 'a'; i <= 'z'; i++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar(i);
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
