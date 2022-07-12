5bc214b8443e23a7ea3e626d4f5e42dbad780f68
Incredibletochi <victor.oraekwuotu@gmail.com>
2022-07-12 15:33:39 -0700

    Task 7777

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,14 @@
  */
 void puts_half(char *str)
 {
-	int len;
+	int x, y, i;
 
-	if (str)
-	{
-		for (len = 0; *(str + len); ++len)
-			for (str += (len + 1) / 2; *str; ++str)
-				_putchar(*str);
-		_putchar('\n');
-	}
+	x = strlen(str);
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2
+			for (i = y; i < x; i++)
+				_putchar(str[i]);
+	_putchar('\n');
 }
