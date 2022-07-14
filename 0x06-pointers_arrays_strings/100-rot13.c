7e030d0c71b40c9fedc57784972d614c9ee60a87
Incredibletochi <victor.oraekwuotu@gmail.com>
2022-07-14 12:09:28 -0700

    Task 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,47 @@
 #include "main.h"
 
 /**
- * print_number - print an integer
- * @n: the integer to print
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
  *
- * Return: void
+ * Return: A pointer to the encoded string.
  */
-void print_number(int n)
+char *rot13(char *str)
 {
-	if (n > -1)
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R',
+			'S', 'T', 'U', 'V', 'W', 'X',
+			'Y', 'Z', 'a', 'b', 'c', 'd',
+			'e', 'f', 'g', 'h', 'i', 'j',
+			'k', 'l', 'm', 'n', 'o', 'p',
+			'q', 'r', 's', 't', 'u', 'v',
+			'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			'T', 'U', 'V', 'W', 'X', 'Y',
+			'Z', 'A', 'B', 'C', 'D', 'E',
+			'F', 'G', 'H', 'I', 'J', 'K',
+			'L', 'M', 'n', 'o', 'p', 'q',
+			'r', 's', 't', 'u', 'v', 'w',
+			'x', 'y', 'z', 'a', 'b', 'c',
+			'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm'};
+
+	while (str[indx1])
 	{
-		if (n > 9)
-			print_number(n / 10);
-		_putchar('0' + n % 10);
+		for (indx2 = 0; indx2 < 52; indx2++)
+		{
+			if (str[indx1] == alphabet[indx2])
+			{
+				str[indx1] = rot13key[indx2];
+				break;
+			}
+		}
+
+		indx1++;
+
+
 	}
-	else
-	{
-		_putchar('-');
-		if (n < -9)
-			print_number(n / -10);
-		_putchar('0' - n % 10);
-	}
+	return (str);
 }
