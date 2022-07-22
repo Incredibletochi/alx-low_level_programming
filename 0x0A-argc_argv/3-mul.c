7b0a6fb8d4ac9b3d64a740cfe55e07479da70eb4
Incredibletochi <victor.oraekwuotu@gmail.com>
2022-07-22 04:16:43 -0700

    Task 33

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,62 +1,27 @@
-#include "main.h"
-#include <limits>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_number - print an integer
- * @n: the integer to print
- *
- * Return: void
+ * main - A program that multiplies two numbers
+ * @argc: The arguments' counter
+ * @argv: The arguments' values
+ * Return: Always 0 (Success)
  */
-void print_number(int n)
+int main(int argc, char **argv)
 {
-	if (n > -1)
+	int num1, num2, result;
+
+	if (argc != 3)
 	{
-		if (n > 9)
-			print_number(n / 10);
-		_putchar('0' + n % 10);
+		printf("Error\n");
+		return (1);
 	}
 	else
 	{
-		_putchar('-');
-		if (n < -9)
-			print_number(n / -10);
-		_putchar('0' - n % 10);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
 	}
-}
-
-
-/**
- * _puts - print a string, followed by a new line
- * @str: the string to print
- *
- * Return: void
- */
-void _puts(char *str)
-{
-	while (*str)
-		_putchar(*str++);
-	_putchar('\n');
-}
-
-
-/**
- * main - print the product of two numbers
- * @argc: size of the argument vector
- * @argv: program name and arguments
- *
- * Return: 1 if called with anything other than 2 arguments,
- * otherwise 0
- */
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		_puts("Error");
-		return (1);
-	}
-
-	print_number(atoi(argv[1]) * atoi(argv[2]));
-	_putchar('\n');
-
-	return (0);
 }
