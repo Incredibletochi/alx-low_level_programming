bca2c26bf127662c3bc75fba6662956447c5cfbc
incredibletochi <victor.oraekwuotu@gmail.com>
2022-10-21 01:30:27 -0700

    Hot Fix Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,29 +1,40 @@
 #include "hash_tables.h"
+#include <string.h>
 
 /**
- * hash_table_get - Retrieve the value associated with
- *                  a key in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to get the value of.
+ * hash_chain_get - get an element from a singly-linked list
+ * @head: a pointer to the singly-linked list
+ * @key: a pointer to the element's key
  *
- * Return: If the key cannot be matched - NULL.
- *         Otherwise - the value associated with key in ht.
+ * Return: a pointer to the element's value, or NULL if the key wasn't found
+ */
+char *hash_chain_get(const hash_node_t *head, const char *key)
+{
+	if (head)
+	{
+		if (strcmp(key, head->key))
+			return (hash_chain_get(head->next, key));
+
+		return (head->value);
+	}
+	return (NULL);
+}
+
+/**
+ * hash_table_get - get an element from a hash table
+ * @ht: a pointer to the hash table
+ * @key: a pointer to the element's key
+ *
+ * Return: a pointer to the element's value, or NULL if the key wasn't found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *node;
-	unsigned long int index;
+	unsigned long int index = 0;
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (NULL);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
-		return (NULL);
-
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
-
-	return ((node == NULL) ? NULL : node->value);
+	if (ht && key && *key)
+	{
+		index = key_index((const unsigned char *) key, ht->size);
+		return (hash_chain_get(ht->array[index], key));
+	}
+	return (0);
 }
